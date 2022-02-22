bf3280498424cb37958bbdd0ab1ada74c4181b57
Franklin Tamborello <frank@Octolen.local>
2022-02-21 18:40:44 -0600

    Generalized dataclean to allow the user to supply a set of additional stopwords for dataclean to filter out of a text.

--- dataclean.py
+++ dataclean.py
@@ -7,7 +7,7 @@
 
 import os, pickle, re, string
 
-def dataclean (text, fedReg=False):
+def dataclean (text, additional_stopwords={}]):
     # Load the NLTK stopwords
     with  open(os.path.join(os.path.dirname(__file__), "stopwords.pkl"), "rb") as f:
         stopwords = pickle.load(f)
@@ -25,14 +25,10 @@ def dataclean (text, fedReg=False):
     text = re.sub('[\W]{2,}', ' ', text)
     
 
-    # Remove stopwords, including Federal Register stopwords
+    # Remove stopwords, including any additional user-supplied stopwords
     # Let's add the following to dataclean's stopwords
-    fedRegStopWords = {'rule', 'rules', 'final', 'federal', 'regulatory', 'filing', 'filings', 'regulation', 'regulations', 'volume', 'volumes', 'register', 'page', 'pages', 'number', 'online', 'government', 'publishing', 'office', 'gpo', 'www', 'gov', 'doc', 'no', 'department', 'cfr', 'part', 'parts', 'section', 'sections', 'title', 'titles', 'pursuant', 'paragraph', 'summary', 'background', 'supplementary', 'information', 'national', 'nation', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'}
-    if fedReg == True:
-        stopwords = stopwords.union(fedRegStopWords)
+    stopwords = stopwords.union(additional_stopwords)
     text = " ".join([word for word in text.split(" ") if word not in stopwords])
     
     return text
 
-# test
-# print(dataclean("How now brown cow?"))
